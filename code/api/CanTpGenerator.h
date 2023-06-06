35c19afe3f2fe8789a93243a51b46fa982f1debd
RhushabhRR <rhushabhraghavendrarr@gmail.com>
2023-06-06 18:22:25 +0200

    Multi frame generation supported

--- code/api/CanTpGenerator.h
+++ code/api/CanTpGenerator.h
@@ -4,11 +4,18 @@
 #include <unordered_map>
 
 #define CANTP_PCI_INFO_OFFSET 0
+#define CANTP_FF_DL_INFO_OFFSET 1
 
 #define CANTP_PCI_SINGLE_FRAME 0
 #define CANTP_PCI_FIRST_FRAME 1
 #define CANTP_PCI_CONSECUTIVE_FRAME 2
 #define CANTP_PCI_FLOWCONTROL_FRAME 3
+
+#define CANTP_MAX_PAYLOAD_LENGTH 4096
+
+#define CANTP_PAYLOAD_BYTES_IN_SF 7
+#define CANTP_PAYLOAD_BYTES_IN_FF 6
+#define CANTP_PAYLOAD_BYTES_IN_CF 7
 
 enum class CanTpFrames : uint8_t
 {
@@ -21,8 +28,8 @@ enum class CanTpFrames : uint8_t
 
 struct CanTpProtocolData
 {
-    uint8_t pciInfo; // Use '+' unary oerator to print uint8_t on console
-    uint16_t maxPayloadLength;
+    uint8_t pciInfo;                                                               // Use '+' unary oerator to print uint8_t on console
+    void (*pfWritePayload)(uint16_t payloadLength, std::vector<uint8_t> &payload); // Function to be called to generate payload
 };
 
 class FrameGenerator
@@ -58,17 +65,12 @@ class CanTpGenerator : public FrameGenerator
 {
 private:
     // Default Parameters
-    uint16_t dataLength = 0;
-    // std::vector<uint8_t> payload;
+    const uint8_t CANTP_FRAME_LENGTH = 8; // not FD
+    uint8_t m_defaultFill = 0xcc;
     std::unordered_map<CanTpFrames, CanTpProtocolData> CanTpFrameFormat;
 
 public:
-    CanTpGenerator()
-    {
-        CanTpFrameFormat[CanTpFrames::CANTP_SINGLE_FRAME] = {CANTP_PCI_SINGLE_FRAME, 7};
-        CanTpFrameFormat[CanTpFrames::CANTP_FIRST_FRAME] = {CANTP_PCI_FIRST_FRAME, 4096};
-        CanTpFrameFormat[CanTpFrames::CANTP_CONSECUTIVE_FRAME] = {CANTP_PCI_CONSECUTIVE_FRAME, 4096};
-    }
+    CanTpGenerator();
 
     ~CanTpGenerator(){};
 
@@ -77,4 +79,26 @@ public:
                                std::vector<uint8_t> &frame) const override;
     virtual bool ReadConfig() override;
     virtual bool SendFrame() override;
+
+    /**
+     * @brief
+     *
+     * @param payloadLength
+     * @param defaultFillValue
+     * @param frame - Pass an empty vector as input
+     * @param seqNum - Used only for consecutive frame
+     * @return true
+     * @return false
+     */
+    bool GenerateFrame(uint16_t payloadLength, std::vector<std::vector<uint8_t>> &frame);
+
+    void SetDefaultFillValue(uint8_t defaultFill)
+    {
+        m_defaultFill = defaultFill;
+    }
+
+private:
+    void GeneratePayload(CanTpFrames frameType, uint16_t payloadLength, std::vector<uint8_t> &payload, uint8_t seqNum = 0xFF);
+
+    uint16_t RequiredFrames(uint16_t msgLength);
 };
--- code/sources/CanTpGenerator.cpp
+++ code/sources/CanTpGenerator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <array>
 #include <cassert>
 
@@ -11,27 +12,16 @@
     payload[0] |= ((0xF & pci_info) << 4)
 
 static void WritePayloadLength(CanTpFrames frameType, std::vector<uint8_t> &payload, uint16_t payloadLength);
+static void GenerateSFpayload() {}
+static void GenerateFFpayload() {}
+static void GenerateCFpayload() {}
+static void GenerateFCpayload() {}
 
-int main()
+CanTpGenerator::CanTpGenerator()
 {
-    CanTpFrames frameType = CanTpFrames::CANTP_FIRST_FRAME;
-    uint16_t payloadLength = 0x122;
-    uint8_t defaultValue = 0x34;
-
-    CanTpGenerator TpGenerator;
-    std::vector<uint8_t> frame;
-    TpGenerator.GenerateFrame(frameType, payloadLength, defaultValue, frame);
-
-    for (auto element : frame)
-    {
-        // if ((element & 0xF0) == 0x2)
-        // {
-        // std::cout << std::endl;
-        // }
-        std::cout << std::hex << +element << ", ";
-    }
-
-    return 0;
+    CanTpFrameFormat[CanTpFrames::CANTP_SINGLE_FRAME] = {CANTP_PCI_SINGLE_FRAME, NULL};
+    CanTpFrameFormat[CanTpFrames::CANTP_FIRST_FRAME] = {CANTP_PCI_FIRST_FRAME, NULL};
+    CanTpFrameFormat[CanTpFrames::CANTP_CONSECUTIVE_FRAME] = {CANTP_PCI_CONSECUTIVE_FRAME, NULL};
 }
 
 bool CanTpGenerator::GenerateFrame(CanTpFrames frameType, uint16_t payloadLength, uint8_t defaultFillValue,
@@ -39,31 +29,30 @@ bool CanTpGenerator::GenerateFrame(CanTpFrames frameType, uint16_t payloadLength
 {
     bool success = false;
 
-    auto itrFrameType = CanTpFrameFormat.find(frameType);
-    if (itrFrameType != CanTpFrameFormat.end())
-    {
-        if (payloadLength <= itrFrameType->second.maxPayloadLength)
-        {
-            std::cout << "Frame filled" << std::endl;
-            frame.resize(CAN_FRAME_LENGTH, defaultFillValue);
-            // Fill the protocol Data
-            frame[0] = 0x00;
-            _WRITE_PCI_INFO(frame.begin(), itrFrameType->second.pciInfo);
-            WritePayloadLength(itrFrameType->first, frame, payloadLength);
+    // auto itrFrameType = CanTpFrameFormat.find(frameType);
+    // if (itrFrameType != CanTpFrameFormat.end())
+    // {
+    //     if (payloadLength <= itrFrameType->second.maxPayloadLength)
+    //     {
+    //         frame.resize(CAN_FRAME_LENGTH, defaultFillValue);
+    //         // Fill the protocol Data
+    //         frame[0] = 0x00;
+    //         _WRITE_PCI_INFO(frame.begin(), itrFrameType->second.pciInfo);
+    //         WritePayloadLength(itrFrameType->first, frame, payloadLength);
 
-            success = true;
-        }
-        else
-        {
-            std::cout << "Payload length too long" << std::endl;
-            std::cout << "Max paylaod length = " << itrFrameType->second.maxPayloadLength << std::endl;
-        }
-    }
+    //         success = true;
+    //     }
+    //     else
+    //     {
+    //         std::cout << "Payload length too long" << std::endl;
+    //         std::cout << "Max paylaod length = " << itrFrameType->second.maxPayloadLength << std::endl;
+    //     }
+    // }
 
-    else
-    {
-        std::cout << "Frame type not found" << std::endl;
-    }
+    // else
+    // {
+    //     std::cout << "Frame type not found" << std::endl;
+    // }
 
     return success;
 }
@@ -76,6 +65,37 @@ bool CanTpGenerator::ReadConfig()
 bool CanTpGenerator::SendFrame()
 {
     return true;
+}
+
+bool CanTpGenerator::GenerateFrame(uint16_t payloadLength, std::vector<std::vector<uint8_t>> &frame)
+{
+    bool success = false;
+
+    uint16_t reqFrames = RequiredFrames(payloadLength);
+    frame.resize(reqFrames, std::vector<uint8_t>(CANTP_FRAME_LENGTH, m_defaultFill));
+
+    if (payloadLength < CANTP_MAX_PAYLOAD_LENGTH)
+    {
+        if (payloadLength < CANTP_PAYLOAD_BYTES_IN_SF)
+        {
+            GeneratePayload(CanTpFrames::CANTP_SINGLE_FRAME, payloadLength, frame[0]);
+        }
+        else
+        {
+            GeneratePayload(CanTpFrames::CANTP_FIRST_FRAME, payloadLength, frame[0]);
+
+            uint8_t seqNum = 0;
+
+            for (uint16_t idx = 1; idx < reqFrames; idx++)
+            {
+                seqNum %= 0x10;
+                GeneratePayload(CanTpFrames::CANTP_CONSECUTIVE_FRAME, payloadLength, frame[idx], seqNum);
+                seqNum += 1;
+            }
+        }
+    }
+
+    return success;
 }
 
 static void WritePayloadLength(CanTpFrames frameType, std::vector<uint8_t> &payload, uint16_t payloadLength)
@@ -101,4 +121,125 @@ static void WritePayloadLength(CanTpFrames frameType, std::vector<uint8_t> &payl
     default:
         break;
     }
+}
+
+uint16_t CanTpGenerator::RequiredFrames(uint16_t msgLength)
+{
+    uint16_t totalFrames = 0;
+    if (msgLength < CANTP_PAYLOAD_BYTES_IN_SF)
+    {
+        totalFrames = 1;
+    }
+    else if (msgLength < CANTP_MAX_PAYLOAD_LENGTH)
+    {
+        uint16_t payloadCounter = 0;
+
+        payloadCounter += CANTP_PAYLOAD_BYTES_IN_FF;
+        totalFrames += 1;
+
+        while (payloadCounter < msgLength)
+        {
+            totalFrames += 1;
+            payloadCounter += CANTP_PAYLOAD_BYTES_IN_CF;
+        }
+    }
+
+    return totalFrames;
+}
+
+void CanTpGenerator::GeneratePayload(CanTpFrames frameType, uint16_t payloadLength, std::vector<uint8_t> &payload, uint8_t seqNum)
+{
+    assert(frameType < CanTpFrames::TOTAL_FRAME_TYPES);
+
+    switch (frameType)
+    {
+    case CanTpFrames::CANTP_SINGLE_FRAME:
+    {
+        auto itrFrameType = CanTpFrameFormat.find(frameType);
+        if (itrFrameType != CanTpFrameFormat.end())
+        {
+            if (itrFrameType->second.pfWritePayload != NULL)
+            {
+                // Custom payload generator
+                itrFrameType->second.pfWritePayload(payloadLength, payload);
+            }
+            else
+            {
+                payload[CANTP_PCI_INFO_OFFSET] &= 0x00; // Clear 1st byte
+                payload[CANTP_PCI_INFO_OFFSET] |= ((itrFrameType->second.pciInfo << 4) & 0xF0);
+                payload[CANTP_PCI_INFO_OFFSET] |= (payloadLength & 0x0F);
+            }
+        }
+    }
+    break;
+
+    case CanTpFrames::CANTP_FIRST_FRAME:
+    {
+        auto itrFrameType = CanTpFrameFormat.find(frameType);
+        if (itrFrameType != CanTpFrameFormat.end())
+        {
+            if (itrFrameType->second.pfWritePayload != NULL)
+            {
+                // Custom payload generator
+                itrFrameType->second.pfWritePayload(payloadLength, payload);
+            }
+            else
+            {
+                payload[CANTP_PCI_INFO_OFFSET] &= 0x00; // Clear 1st byte
+                payload[CANTP_PCI_INFO_OFFSET] |= ((itrFrameType->second.pciInfo << 4) & 0xF0);
+                payload[CANTP_PCI_INFO_OFFSET] |= ((payloadLength >> 8) & 0x0F);
+
+                payload[CANTP_FF_DL_INFO_OFFSET] &= 0x00; // Clear 2nd byte
+                payload[CANTP_FF_DL_INFO_OFFSET] |= ((payloadLength)&0xFF);
+            }
+        }
+    }
+    break;
+
+    case CanTpFrames::CANTP_CONSECUTIVE_FRAME:
+    {
+        auto itrFrameType = CanTpFrameFormat.find(frameType);
+        if (itrFrameType != CanTpFrameFormat.end())
+        {
+            if (itrFrameType->second.pfWritePayload != NULL)
+            {
+                // Custom payload generator
+                itrFrameType->second.pfWritePayload(payloadLength, payload);
+            }
+            else
+            {
+                assert(seqNum <= 0xF);
+                // TODO - resize only if vector is empty
+                payload[CANTP_PCI_INFO_OFFSET] &= 0x00; // Clear 1st byte
+                payload[CANTP_PCI_INFO_OFFSET] |= ((itrFrameType->second.pciInfo << 4) & 0xF0);
+                payload[CANTP_PCI_INFO_OFFSET] |= (seqNum & 0x0F);
+            }
+        }
+    }
+    break;
+
+    default:
+        break;
+    }
+}
+
+int main()
+{
+    CanTpGenerator generator;
+    uint16_t payloadLength = 0xffe;
+    std::vector<std::vector<uint8_t>> payload;
+
+    generator.SetDefaultFillValue(0x45);
+    generator.GenerateFrame(payloadLength, payload);
+
+    for (auto &frame : payload)
+    {
+        for (auto &data : frame)
+        {
+            std::cout << "0x" << std::setfill('0') << std::setw(2) << std::hex << +data << ", ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
 }
